2357da3fa41f094627f055695acdb2bd6c04a9e5
GoG_Paulie <ppolih21@gmail.com>
2020-09-27 17:46:02 -0400

    Updates

---
+++ precache-manifest.176d415504e8617e6d0b552773108cc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98df646e91e18ac62bf7048301f46f8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1c04cd93cff168cc979",
+    "url": "/static/css/main.853c1f6f.chunk.css"
+  },
+  {
+    "revision": "c599ef294cbd12297255",
+    "url": "/static/js/2.97f0c7a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.97f0c7a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1c04cd93cff168cc979",
+    "url": "/static/js/main.911e6a82.chunk.js"
+  },
+  {
+    "revision": "c8aed86471fdb6de3cef",
+    "url": "/static/js/runtime-main.95474e96.js"
+  },
+  {
+    "revision": "b410fd8276fbf93de33f4eb666e04e5c",
+    "url": "/static/media/spinner.b410fd82.gif"
+  }
+]);
